d04dc00e45bd06a8528fdb22f2551536f6558508
IvanG <kikin-gr2000@hotmail.com>
2023-01-21 01:20:45 -0600

    End of clase 16

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,8 +1,7 @@
 import { List, ListItem, Typography } from '@mui/material';
 import React from 'react'
 import { Category } from '../Category'
-import axios from 'axios';
-// import db from '../../api/db.json'
+import './ListOfCategories.css'
 
 const listStyle = {
   display: 'flex',
@@ -11,16 +10,25 @@ const listStyle = {
   '&::-webkit-scrollbar': {
     display: 'none'
   },
+  '&.fixed': {
+    position: 'fixed',
+    top: '0px',
+    width: '100%',
+    backgroundColor: '#fefefe',
+    borderRadius: '0 0 5px 5px',
+    boxShadow: '0px 14px 29px -26px rgba(10, 10, 10, 0.74)',
+    zIndex: '1',
+    padding: '5px',
+    maxWidth: '500px',
+  },
 };
 const listItemStyle = {
   padding: '0 0.5rem',
-  [ '@media (min-width: 700px)' ]: {
-    // padding: '0 1rem',
-  },
 };
 
 const ListOfCategories = () => {
   const [ categories, setCategories ] = React.useState([]);
+  const [ showFixed, setShowFixed ] = React.useState(false);
 
   React.useEffect(() => {
     const fetchCategories = async () => {
@@ -31,8 +39,24 @@ const ListOfCategories = () => {
     fetchCategories();
   }, []);
 
-  return (
-    <List sx={listStyle} disablePadding={true}>
+  React.useEffect(() => {
+    const onScroll = (e) => {
+      const newShowFixed = window.scrollY > 130;
+      newShowFixed !== showFixed && setShowFixed(newShowFixed);
+    };
+
+    document.addEventListener('scroll', onScroll);
+
+    return () => document.removeEventListener('scroll', onScroll);
+  }, [ showFixed ]);
+
+  const renderList = (fixed, isCopy) => (
+    <List
+      className={(fixed ? 'fixed' : '')}
+      sx={listStyle}
+      disablePadding={true}
+      id={isCopy ? 'listOfCategories' : ''}
+    >
       {
         categories.length > 0 && (
           categories.map(category =>
@@ -50,6 +74,12 @@ const ListOfCategories = () => {
         <Typography variant='body1'>Loading...</Typography>
       }
     </List>
+  );
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true, true)}
+    </>
   )
 };
 
