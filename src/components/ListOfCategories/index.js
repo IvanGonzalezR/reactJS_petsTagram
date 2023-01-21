20d2c5c4698a280eaf2fac46dc1bc178281c23e7
IvanG <kikin-gr2000@hotmail.com>
2023-01-20 23:04:26 -0600

    End of clase 21

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,7 +1,8 @@
-import { List, ListItem } from '@mui/material';
+import { List, ListItem, Typography } from '@mui/material';
 import React from 'react'
 import { Category } from '../Category'
-import db from '../../api/db.json'
+import axios from 'axios';
+// import db from '../../api/db.json'
 
 const listStyle = {
   display: 'flex',
@@ -19,20 +20,33 @@ const listItemStyle = {
 };
 
 const ListOfCategories = () => {
+  const [ categories, setCategories ] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchCategories = async () => {
+      const response = await fetch('https://petgram-server-24iykciv5.now.sh/categories');
+      const data = await response.json();
+      setCategories(data);
+    }
+    fetchCategories();
+  }, []);
   return (
     <List sx={listStyle} disablePadding={true}>
       {
-        db.categories.map(category =>
-          <ListItem
-            key={category.id}
-            disablePadding={true}
-            sx={listItemStyle}
-          >
-            <Category
-              {...category}
-            />
-          </ListItem>
-        )
+        categories.length > 0 && (
+          categories.map(category =>
+            <ListItem
+              key={category.id}
+              disablePadding={true}
+              sx={listItemStyle}
+            >
+              <Category
+                {...category}
+              />
+            </ListItem>
+          ))
+        ||
+        <Typography variant='body1'>Loading...</Typography>
       }
     </List>
   )
