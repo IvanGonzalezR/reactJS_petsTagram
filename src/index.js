2d1ea00bb8d783d7b243ac798b87fbeed4e9afa3
IvanG <kikin-gr2000@hotmail.com>
2023-01-25 01:13:17 -0600

    End of clase 20.23

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+
+const client = new ApolloClient({
+  uri: 'https://petgram-server-24iykciv5.now.sh/graphql',
+  cache: new InMemoryCache()
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App className="App" />
+  <ApolloProvider client={client}>
+    <App className="App" />
+  </ApolloProvider>
 );
