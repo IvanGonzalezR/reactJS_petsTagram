40058f78869a88cfc022e5d2e55113a45042c07c
IvanG <kikin-gr2000@hotmail.com>
2023-01-30 19:24:06 -0600

    End of clase 31

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import { Logo } from './components/Logo';
 import { HomePage } from './pages/HomePage';
 import { ThemeProvider } from '@mui/material';
 import { theme } from './styles/GlobalTheme';
-import { Detail } from './pages/Detail';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { NavBar } from './components/NavBar';
+import { Detail } from './pages/Detail';
+import { User } from './pages/User';
+import { Favs } from './pages/Favs';
+import { NotRegisteredUser } from './pages/NotRegisteredUser';
+
+
+const isAuth = false;
 
 function App() {
-
   return (
     <>
       <ThemeProvider theme={theme}>
@@ -19,6 +24,16 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/pet/:petId" element={<HomePage />} />
             <Route path="/detail/:detailId" element={<Detail />} />
+            <Route path="/favorites" element={
+              isAuth
+                ? <Favs />
+                : <NotRegisteredUser />
+            } />
+            <Route path="/user" element={
+              isAuth
+                ? <User />
+                : <NotRegisteredUser />
+            } />
             <Route path="*" element={<h1>404 Not Found!</h1>} />
           </Routes>
           <NavBar />
